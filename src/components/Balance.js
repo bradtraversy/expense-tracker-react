d1f578ae156b5ed61f4891cebc1debb4fe39de2c
spin <spindog.exe@gmail.com>
2020-03-05 14:21:38 +0800

    Destructure transaction amounts

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ import { GlobalContext } from '../context/GlobalState';
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
-  const amounts = transactions.map(transaction => transaction.amount);
+  const amounts = transactions.map(({ amount }) => amount);
 
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -4,7 +4,7 @@ import { GlobalContext } from '../context/GlobalState';
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
 
-  const amounts = transactions.map(transaction => transaction.amount);
+  const amounts = transactions.map(({ amount }) => amount);
 
   const income = amounts
     .filter(item => item > 0)
