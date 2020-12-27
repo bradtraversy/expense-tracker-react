e1ab09697546926dc6117b3afacb48245ed31bd5
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2020-12-27 21:09:39 +0300

    added money formatter function

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,6 +1,22 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
+
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
 
@@ -8,23 +24,22 @@ export const IncomeExpenses = () => {
 
   const income = amounts
     .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+    .reduce((acc, item) => (acc += item), 0);
 
   const expense = (
     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
-  ).toFixed(2);
+  );
 
   return (
     <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-  <p className="money plus">{income}</p>
+  <p className="money plus">{moneyFormatter(income)}</p>
         </div>
         <div>
           <h4>Expense</h4>
-  <p className="money minus">{expense}</p>
+  <p className="money minus">{moneyFormatter(expense)}</p>
         </div>
       </div>
   )
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,22 @@
 import React, {useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
+
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
 
@@ -8,7 +24,7 @@ export const Transaction = ({ transaction }) => {
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+      {transaction.text} <span>{sign}{moneyFormatter(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
   )
 }
