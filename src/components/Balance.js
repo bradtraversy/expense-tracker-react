8d419b11f7f71a5aad67dac60540faded376b416
Samuel Suther <info@suther.de>
2021-10-20 07:42:47 +0200

    Fix missing pre-sign
    If value is negative, you don't return the minus in front of your balance. 
    This PR fix this.

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,7 +5,7 @@ import { GlobalContext } from '../context/GlobalState';
 function moneyFormatter(num) {
   let p = num.toFixed(2).split('.');
   return (
-    '$ ' +
+    '$ ' + (p[0].split('')[0]=== '-' ? '-' : '') +
     p[0]
       .split('')
       .reverse()
