4ecee34b6588fe2e5625e77e1e9af1f0d40e2dab
aainasantos <103295944+aainasantos@users.noreply.github.com>
2022-06-06 05:19:13 +0800

    adding some condition for sorting the history

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,13 +5,47 @@ import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
+  const [filter, setFilter] = useState();
 
+  // added filtering
+
+  const incomes = transactions.filter(transaction => {
+    return transaction.amount > 0;
+  });
+
+  const expenses = transactions.filter(transaction => {
+    return transaction.amount < 0;
+  });
+
+  const all = transactions.map(transaction => {
+    return transaction
+  });
+
+  function handleChange(e){
+    const selectFilter = e.target.value;
+    setFilter(selectFilter);
+  }
+  
   return (
     <>
-      <h3>History</h3>
+      <h3>History    
+        {/* add this Select for sorting */}
+        <select name="cars" id="cars" onChange={handleChange}>
+          <option value="Income">Sort By</option>
+          <option value="Income">Income</option>
+          <option value="Expense">Expense</option>
+          <option value="All">All</option>
+        </select>
+      </h3>
+    {/* add some condition */}
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      {(filter === "Income"
+        ? incomes
+        : filter === "Expense"
+        ? expenses
+        : all).map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
+      
     </>
   )
 }
