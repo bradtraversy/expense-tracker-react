c09e009f94e9b3730803e37f9547cfa6e14875f1
Rahul Patil <rahulvpatil2003@gmail.com>
2023-10-30 01:19:10 +0530

    Added some features
    Rupee Icon,
    Delete option,
    0 error solved

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,7 +5,7 @@ import { GlobalContext } from '../context/GlobalState';
 function moneyFormatter(num) {
   let p = num.toFixed(2).split('.');
   return (
-    '$ ' +
+    '₹ ' +
     p[0]
       .split('')
       .reverse()
